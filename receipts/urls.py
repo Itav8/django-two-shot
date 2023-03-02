cfc6844ac5847d76bbfc874673cd5f26ea812f02
Italiz <italizv@gmail.com>
2023-03-02 14:05:59 -0500

    Feature 5 complete

---
+++ receipts/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from receipts.views import show_receipt
+
+
+urlpatterns = [
+    path("", show_receipt, name="home"),
+]
--- receipts/views.py
+++ receipts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+from receipts.models import Receipt
+
+
 # Create your views here.
+# def show_receipt(request):
+#     receipts = Receipt.objects.all()
+#     context = {
+#         "receipt_list": receipts,
+#     }
+#     return render(request, "receipts/receipt_list.html", context)
+
+
+def show_receipt(request):
+    receipts = Receipt.objects.all()
+    context = {
+        "receipt_list": receipts,
+    }
+    return render(request, "receipts/receipt_list.html", context)
